014da161d14fa80c13f34039decc875fe59135ab
John Bender <john.m.bender@gmail.com>
2015-10-12 21:56:39 -0700

    suggested modifications from Jens

--- code/TxCommit.c
+++ code/TxCommit.c
@@ -1,19 +1,19 @@
-...
+  ...
 
-# ifndef TL2_EAGER
-for (wr = logs; wr != end; wr++) {
-  // write the deferred stores
-  WriteBackForward(wr);
-}
-# endif
+  # ifndef TL2_EAGER
+  for (wr = logs; wr != end; wr++) {
+    // write the deferred stores
+    WriteBackForward(wr);
+  }
+  # endif
 
-// make stores visible before unlock
-MEMBARSTST();
+  // make stores visible before unlock
+▶ MEMBARSTST();
 
-// release locks and increment version
-DropLocks(Self, wv);
+  // release locks and increment version
+  DropLocks(Self, wv);
 
-// ensure later loads from above writes
-MEMBARSTLD();
+  // ensure later loads from above writes
+▶ MEMBARSTLD();
 
-...
+  ...
